29286c53d99b64b8ab8ee85b30c522f718b373d0
biabonadia <beatrizbonadia@gmail.com>
2021-02-14 10:32:57 -0500

    deleted unecessary files & installed bootstrap

---
+++ src/Weather.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export default function Weather(){
+return (
+
+<div>
+    
+    Good morning
+    
+    <button className="btn btn-primary">Abuse me</button>
+    </div>); 
+
+
+
+
+
+
+}
