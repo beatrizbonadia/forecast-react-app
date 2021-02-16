53fae9f501c05d63cc0a703791bf04626c70055f
biabonadia <beatrizbonadia@gmail.com>
2021-02-16 10:59:46 -0500

    Added row,col and CSS properties for different elements

--- src/Weather.js
+++ src/Weather.js
@@ -1,13 +1,44 @@
 import React from 'react';
-
+import "./Weather.css";
 export default function Weather(){
 return (
 
-<div>
+<div className="Weather">
+<form className="row form">
+    <input className="col-4 searchCity" type="search" placeholder="Enter a city" />
+    <input className="col-2 btn btn-primary submit" type="submit" value="Search"/>
+    <button className="col-2 btn btn-default celsius">℃</button>
+    <button className="col-2 btn btn-default farh">℉</button>
+    <button className="col-2 btn btn-warning location">📍 Location</button>
+</form>
+
+    <h1>Tokyo</h1>
+    <ul>
+        <li className="todayInfo">Sunday, FEB 14</li>
+        <li className="time">10:45 AM</li>
+        
+    </ul>
+
+    <div className="row mainInfo">
+<div className="col-xs-8  main">
+<img className=" icon" src="https://icons.iconarchive.com/icons/icons-land/weather/256/Sunny-icon.png" alt="Sunny" />
+
+
+<strong className="col-xs-4 temperature" >24°</strong>
+</div>
+</div>
+    <div className="row weatherLooks">Sunny</div>
     
-    Good morning
+<div  className="row weatherInfo">
+<div className="col-xs-6 col-sm-4 maxMin">28° | 22°</div>
+<div className="col-xs-6 col-sm-4 wind">Wind: 5 km/h</div>
+<div className="col-xs-6 col-sm-4 humidity">Humidity: 72%</div>
+</div>
+
     
-    <button className="btn btn-primary">Abuse me</button>
+
+    
+  
     </div>); 
 
 
