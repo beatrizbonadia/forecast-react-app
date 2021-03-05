ecd61a17b887fbcedc764daf97dcf96b0986960d
biabonadia <beatrizbonadia@gmail.com>
2021-03-05 11:23:54 -0500

    Adjusted forecast icon

--- src/ForecastSection.js
+++ src/ForecastSection.js
@@ -5,9 +5,9 @@ import "./ForecastSection.css";
 export default function ForecastSection(props){
 return(
  <div className="col forecastSection">
-        <p>{new Date(props.info.dt*1000).getHours()}:00</p>
+        <p className="forecastTime">{new Date(props.info.dt*1000).getHours()}:00</p>
         <WeatherIcon code={props.info.weather[0].icon}  className="iconForecast"/>
-       <p> {Math.round(props.info.main.temp)}℃</p>
+       <p className="forecastTemp"> {Math.round(props.info.main.temp)}°</p>
         </div>
 
 );
